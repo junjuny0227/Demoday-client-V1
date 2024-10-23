3121bfbe9c990c21f51eb78d6501da998ec8bff3
송재욱 <123460320+976520@users.noreply.github.com>
2024-10-23 22:48:21 +0900

    service file

---
+++ src/services/SignInController.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+const API_URL = "https://port-0-demoday-server-v1-lzsaeexf05f2c47e.sel4.cloudtype.app/api/v1/auth";
+
+export const SigninController = {
+  signin: async (email: string, password: string) => {
+    try {
+      if (!email) {
+        throw new Error("아이디를 입력해주세요");
+      }
+
+      const response = await axios.post(`${API_URL}/signin`, { email, password });
+
+      if (response.status !== 200) {
+        throw new Error(`error : ${response.statusText}`);
+      }
+
+      return response.status === 200;
+    } catch (error) {
+      throw new Error(`error : ${(error as Error).message}`);
+    }
+  },
+};
