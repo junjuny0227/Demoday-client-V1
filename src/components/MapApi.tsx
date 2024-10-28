cf152a69ca80d790d0bd7472b492754549fd1ffd
송재욱 <haensol976520@gmail.com>
2024-10-28 09:59:12 +0900

    refector code

--- src/components/MapApi.tsx
+++ src/components/MapApi.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
-import loadKakaoMapsSDK from "../services/MapRequest";
+import loadKakaoMapsSDK from "../services/LoadKakaoSDK";
 
-//kakao.d.ts
+// kakao.d.ts
 export interface LatLng {
   lat: number;
   lng: number;
@@ -20,7 +20,7 @@ declare global {
   interface Window {
     kakao: {
       maps: {
-        LatLng: new (lat: number, lng: number) => LatLng; // 리턴 타입을 LatLng 인터페이스로 명시
+        LatLng: new (lat: number, lng: number) => LatLng;
         Map: new (container: HTMLElement, options: MapOptions) => Map;
         load: (callback: () => void) => void;
       };
@@ -28,32 +28,35 @@ declare global {
   }
 }
 
-function MapApi() {
+const MapApi: React.FC = () => {
   const mapRef = useRef<Map | null>(null);
 
-  const initMap = () => {
-    const container = document.getElementById("map");
-    if (!window.kakao || !window.kakao.maps) {
-      console.error("Kakao Maps SDK not available.");
-      return;
-    }
+  useEffect(() => {
+    const initMap = async () => {
+      try {
+        await loadKakaoMapsSDK();
+        const container = document.getElementById("map");
+        if (!container || !window.kakao || !window.kakao.maps) {
+          console.error("Kakao Maps SDK not available or map container not found.");
+          return;
+        }
 
-    const options = {
-      center: new window.kakao.maps.LatLng(37.483034, 126.902435),
-      level: 3,
+        const options: MapOptions = {
+          center: new window.kakao.maps.LatLng(37.483034, 126.902435),
+          level: 3,
+        };
+
+        const map: Map = new window.kakao.maps.Map(container as HTMLElement, options);
+        mapRef.current = map;
+      } catch (error) {
+        console.error("Failed to load Kakao Maps:", error);
+      }
     };
 
-    const map = new window.kakao.maps.Map(container as HTMLElement, options);
-    mapRef.current = map;
-  };
-
-  useEffect(() => {
-    loadKakaoMapsSDK()
-      .then(initMap)
-      .catch((error) => console.error("Failed to load Kakao Maps:", error));
+    initMap();
   }, []);
 
   return <div id="map" style={{ width: "500px", height: "400px" }}></div>;
-}
+};
 
 export default MapApi;
