1de51e76495ceb9899048497ecf7c9add27e081b
junjuny0227 <junjuny0227@gmail.com>
2024-10-28 10:08:31 +0900

    Styling signup button

--- src/pages/SignupPassword.tsx
+++ src/pages/SignupPassword.tsx
@@ -1,6 +1,7 @@
 import { useOutletContext } from "react-router-dom";
 import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
+import styled from "styled-components";
 
 const SignupPassword: React.FC = () => {
   const {
@@ -19,9 +20,24 @@ const SignupPassword: React.FC = () => {
     error: string;
   }>();
 
-  const handleNext = () => {
-    handleSignup();
-  };
+  const Button = styled.button<{ disabled?: boolean }>`
+    display: flex;
+    width: 22.75rem;
+    height: 3.25rem;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    background: ${({ disabled }) => (disabled ? "#e9e8e7" : "#2EA1E9")};
+    color: ${({ disabled }) => (disabled ? "#625d5b" : "#FAFAFA")};
+    font-size: 1rem;
+    font-weight: 500;
+    margin-bottom: 8px;
+    font-family: SUIT;
+    cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+  `;
+
+  const isDisabled =
+    password !== confirmPassword || !password || !confirmPassword;
 
   return (
     <Wrapper>
@@ -39,7 +55,9 @@ const SignupPassword: React.FC = () => {
         placeholder="비밀번호를 확인하세요"
       />
       {error && <p className="error">{error}</p>}
-      <button onClick={handleNext}>가입하기</button>
+      <Button onClick={handleSignup} disabled={isDisabled}>
+        회원가입
+      </Button>
     </Wrapper>
   );
 };
