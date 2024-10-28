8f7e798fcbc87b31be66a10aec029e78384f2580
송재욱 <haensol976520@gmail.com>
2024-10-28 23:24:31 +0900

    publish-settingpage

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -14,8 +14,14 @@ const SettingSection = styled.section`
 `;
 
 const SettingMenu = styled.div`
+  display: flex;
+  flex-direction: row;
   font-size: 16px;
   margin: 12px 24px;
+  p {
+    font-size: 13px;
+    color: #625d5b;
+  }
 `;
 
 const Setting = () => {
@@ -23,7 +29,10 @@ const Setting = () => {
     <>
       <SettingWrapper>
         <SettingSection>개인 설정</SettingSection>
-        <SettingMenu>글꼴 크기 설정</SettingMenu>
+        <SettingMenu>
+          글꼴 크기 설정
+          <p>현재 글꼴 크기</p>
+        </SettingMenu>
         <SettingMenu>워치 연결 상태</SettingMenu>
         <SettingMenu style={{ marginBottom: "32px" }}>테마</SettingMenu>
         <SettingSection>계정 보안</SettingSection>
