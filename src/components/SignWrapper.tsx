62d7317a547a2fe203642e3073e281071f57ca14
junjuny0227 <junjuny0227@gmail.com>
2024-10-29 18:17:42 +0900

    feat: add SignWrapper

---
+++ src/components/SignWrapper.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SignWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  top: 16.75rem;
+  position: absolute;
+`;
